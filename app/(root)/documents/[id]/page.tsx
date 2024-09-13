d9eb2acf3acf6b07438b465e24db57e4c41c27d2
gokul-saaragh <gokulchandan24@gmail.com>
2024-09-14 00:18:04 +0530

    changes made

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { Editor } from "@/components/editor/Editor";
 import React from "react";
+import Header from "@/components/Header";
 
 const Document = () => {
   return (
     <>
       <div>
+        <Header className="sticky left-0 top-0">
+          <div className="flex items-center w-fit justify-center gap-2">
+            <p className="document-title">this is a fake document title</p>
+          </div>
+        </Header>
         <Editor />
       </div>
     </>
