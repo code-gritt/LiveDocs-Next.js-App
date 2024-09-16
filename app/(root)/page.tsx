274ad6b6e12adba37b2e914bf12fe8d227519ce3
gokul-saaragh <gokulchandan24@gmail.com>
2024-09-17 00:37:01 +0530

    app finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import { DeleteModal } from "@/components/DeleteModal";
 import Header from "@/components/Header";
+import Notifications from "@/components/Notifications";
 import { getDocuments } from "@/lib/actions/room.actions";
 import { dateConverter } from "@/lib/utils";
 import { SignedIn, UserButton } from "@clerk/nextjs";
@@ -17,35 +18,18 @@ const Home = async () => {
     clerkUser.emailAddresses[0].emailAddress
   );
 
-  const documents = [];
   return (
     <>
       <main className="home-container">
         <Header className="sticky left-0 top-0">
           <div className="flex items-center gap-2 lg:gap-4">
+            <Notifications />
             <SignedIn>
               <UserButton />
             </SignedIn>
           </div>
         </Header>
 
-        {/* {documents.length > 0 ? (
-          <div></div>
-        ) : (
-          <div className="document-list-empty">
-            <Image
-              src="/assets/icons/doc.svg"
-              alt="Document"
-              width={40}
-              height={40}
-              className="mx-auto"
-            />
-            <AddDocumentBtn
-              userId={clerkUser.id}
-              email={clerkUser.emailAddresses[0].emailAddress}
-            />
-          </div>
-        )} */}
         {roomDocuments.data.length > 0 ? (
           <div className="document-list-container">
             <div className="document-list-title">
